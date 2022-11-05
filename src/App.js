30b14a821a3b8c04badab1ec722228d663435ebd
manav <manavjtanna@gmail.com>
2022-11-06 02:23:29 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import {useState} from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+
+const ops = ['/', '+', '*', '-', '0'];
+
+const updateCalc = value => {
+if (ops.includes(value) && calc === "" || ops.includes(value) && ops.includes(calc.slice(-1))) {
+  return;
+  
+}
+
+
+  setCalc(calc+value);
+
+  if (!ops.includes(value)) {
+    setResult(eval(calc+value).toString());
+  }
+}
+// {result ? <span>{result}</span> :'' }
+
+const equalsTo = () => {
+  setResult(eval(calc).toString());
+  setCalc(" ")
+}
+
+const deleteButton = ()=> {
+  if(calc == ''){
+    return;
+  }
+  const value = calc.slice(0, -1);
+  setCalc(value);
+}
+
+const clearDisplay = () => {
+  setResult("");
+  setCalc("");
+}
+return(
+  
+    <div className = "calc-grid">
+      <div className = "output">
+        <div className = "previous">{calc || "0"}</div>
+        <div className = "current">{result}</div>
+      </div>
+     <button className = "long-btn" onClick={clearDisplay}>AC</button>
+     <button onClick={deleteButton}>DEL</button>
+     <button onClick={()=> updateCalc('+')}>+</button>
+     <button onClick={()=> updateCalc('1')}>1</button>
+     <button onClick={()=> updateCalc('2')}>2</button>
+     <button onClick={()=> updateCalc('3')}>3</button>
+     <button onClick={()=> updateCalc('-')}>-</button>
+     <button onClick={()=> updateCalc('4')}>4</button>
+     <button onClick={()=> updateCalc('5')}>5</button>
+     <button onClick={()=> updateCalc('6')}>6</button>
+     <button onClick={()=> updateCalc('*')}>*</button>
+     <button onClick={()=> updateCalc('7')}>7</button>
+     <button onClick={()=> updateCalc('8')}>8</button>
+     <button onClick={()=> updateCalc('9')}>9</button>
+     <button onClick={()=> updateCalc('/')}>/</button>
+     <button onClick={()=> updateCalc('.')}>.</button>
+     <button onClick={()=> updateCalc('0')}>0</button>
+     <button className = "long-btn" onClick={equalsTo}>=</button>
     </div>
-  );
+
+  
+); 
 }
 
 export default App;
